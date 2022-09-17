d2e86a065b42aeff4951c4177b27dd4fd3718906
Natur Mustafa <mustafa.natur@mail.ru>
2022-09-17 13:08:53 +0300

    master

---
+++ C++/Reverse string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for (int i = 0; i < s.size() / 2; i ++) {
+            char temp = s[i];
+            s[i] = s[s.size() - 1 - i];
+            s[s.size() - 1 - i] = temp;
+        }
+    }
+};
